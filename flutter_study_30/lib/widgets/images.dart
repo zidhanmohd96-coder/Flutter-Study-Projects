68f0b1980328663bcb7bdd931c55c77e3385205b
Zidhan Muhammed <zidhanmohd96@gmail.com>
2026-04-10 16:51:29 +0530

    Day5 Image Widgets

--- flutter_study_30/lib/widgets/images.dart
+++ flutter_study_30/lib/widgets/images.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ImageWidget extends StatelessWidget {
   const ImageWidget({super.key});
@@ -7,7 +8,7 @@ class ImageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Drawer Widget'),
+        title: Text('Image Widgets'),
         elevation: 8,
         backgroundColor: Colors.indigo,
       ),
@@ -15,15 +16,23 @@ class ImageWidget extends StatelessWidget {
         child: Container(
           width: 300,
           height: 350,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: NetworkImage(
+          child: CachedNetworkImage(
+            imageUrl:
                 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Henry_Cavill_at_the_2009_Tribeca_Film_Festival.jpg/250px-Henry_Cavill_at_the_2009_Tribeca_Film_Festival.jpg',
-              ),
-              fit: BoxFit.cover,
-            ),
+            placeholder: (context, url) =>
+                Center(child: CircularProgressIndicator()),
+            errorWidget: (context, url, error) => Icon(Icons.error),
           ),
+          // decoration: BoxDecoration(
+          //   borderRadius: BorderRadius.circular(20),
+          //   image: DecorationImage(
+          //     // image: NetworkImage(
+          //     //   'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Henry_Cavill_at_the_2009_Tribeca_Film_Festival.jpg/250px-Henry_Cavill_at_the_2009_Tribeca_Film_Festival.jpg',
+          //     // ),
+          //     image: AssetImage('assets/cv_image.png'),
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
           // child: Image.network(
           //   'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Henry_Cavill_at_the_2009_Tribeca_Film_Festival.jpg/250px-Henry_Cavill_at_the_2009_Tribeca_Film_Festival.jpg',
           //   fit: BoxFit.cover,
