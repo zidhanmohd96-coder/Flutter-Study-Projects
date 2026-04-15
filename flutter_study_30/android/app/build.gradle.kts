2fda2f17d394a34a1f9ecc43f837ee84421fb710
Zidhan Muhammed <zidhanmohd96@gmail.com>
2026-04-15 12:09:45 +0530

    Day10 Renaming Application

--- flutter_study_30/android/app/build.gradle.kts
+++ flutter_study_30/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_study_30"
+        applicationId = "com.zilzilastudy.flutterStudy30"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
