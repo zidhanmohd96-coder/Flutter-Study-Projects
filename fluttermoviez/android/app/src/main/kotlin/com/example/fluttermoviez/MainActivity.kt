15699ccac0acff8303ab9b6278760e808fe3a864
Zidhan Muhammed <zidhanmohd96@gmail.com>
2026-04-18 14:36:45 +0530

    Creating fluttermoviez

---
+++ fluttermoviez/android/app/src/main/kotlin/com/example/fluttermoviez/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fluttermoviez
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ fluttermoviez/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Moviez')),
+        body: const Center(child: Text('Welcome to Flutter Moviez!')),
+      ),
+    );
+  }
+}
